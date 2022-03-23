b50c7f06097f2afc0490cb4e8f26d2ea793f88e2
Nechama Galili <ntgalili@gmail.com>
2022-03-23 13:07:16 +0200

    fixing

--- Desktop/exellenteam_python/its_the_way.py
+++ Desktop/exellenteam_python/its_the_way.py
@@ -1,9 +1,24 @@
 
 import os
-path=input("enter a path of directory: ")
-list_of_files=os.listdir(path)
-deep_file=[]
-for f in list_of_files:
-    if len(f)>4 and f[:4]=='deep':
-        deep_file+=[f]
-print(deep_file)
+
+
+def file_start(path :str,start:str):
+    """
+    the function get a path of a directory and find the files in the directory that start with start
+    :param path: the path of the directory
+    :param start: the str that the file's names start with
+    :return: list of files
+    """
+
+    list_of_files = os.listdir(path) # get the list of the files in the directory
+    starts_files = [f for f in list_of_files if f.startswith(start)] # get the list of the files that start with start
+    return starts_files
+
+def main():
+    path = input("Enter a path of directory: ")
+    deep_file = file_start(path,'deep') # find the files in the directory that start with 'deep'
+    print(deep_file)
+
+
+if __name__ == "__main__":
+    main()
